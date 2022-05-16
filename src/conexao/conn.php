907e0533f4d87264c6f47c2f9934f9e9841dac41
Victor-Sampaio <victor.sampaio0705@gmail.com>
2022-05-16 00:41:03 -0300

    Commit da aula 01

---
+++ src/conexao/conn.php
@@ -0,0 +1,15 @@
+<?php
+
+//declarar as variáveis necessarias para conexão com o banco de dados.
+$hostname = "sql311.epizy.com";
+$dbname = "epiz_31734008_cantina";
+$username = "epiz_31734008";
+$password = "4sAiJG9WhHKCp";
+
+try {
+    $pdo = new PDO('mysql:host='.$hostname.';dbname', $dbname, $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo 'conexão com o banco de dados conectado';
+}catch(PDOException $e){
+    echo 'Erro' . $e->getMessage();
+}
